a44ffaaa001ac97fc7295e5dc96d66e5fd39030d
hayley-leblanc <leblan_h1@denison.edu>
2020-09-07 11:03:58 -0500

    Do IOCTL in nova checkpoint

--- code/tests/seq1/novaCheckpoint.cpp
+++ code/tests/seq1/novaCheckpoint.cpp
@@ -1,7 +1,32 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+
+#include "../../../../nova-tester/ioctl.h"
 #include "novaCheckpoint.h"
 
 int novaCheckpoint() {
-    // see if you can use this as a nop function that just exists for the 
-    // logger to determine when we want a crash to occur
+    int val = 0;
+    int fd;
+
+    fd = open("/dev/ioctl_dummy", 0);
+    if (fd < 0) {
+        perror("open");
+        return fd;
+    }
+
+    val = ioctl(fd, LOGGER_CHECKPOINT, NULL);
+    if (val < 0) {
+        perror("LOGGER_CHECKPOINT");
+        close(fd);
+        return val;
+    }
+
+    return 0;
+
     return 0;
 }
